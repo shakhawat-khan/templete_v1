b70a9ad2c226278dbcc0f8d3d5ccc1b6d0fd596b
shakhawat-khan <satanfromhelll666@gmail.com>
2023-02-18 17:31:57 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,77 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar/isar.dart';
+import 'package:templete/db/isar/services/isar_services.dart';
+import 'package:templete/db/isar/settings.dart';
+import 'package:templete/db/isar_db.dart';
 import 'package:templete/provider/functions.dart';
 import 'package:templete/theme/theme.dart';
 
-void main() {
+void main() async {
+  final settings = Settings(theme: 'true', id: 1, isDark: true);
+  db = await Isar.open([SettingsSchema], inspector: !kReleaseMode);
+
+  final String boolValue;
+
+  //   final settings = Settings(theme: 'true', id: 1);
+
+  // await db.writeTxn(() => db.settings.put(settings));
+  final test = await db.settings.get(1);
+  if (test == null) {
+    await db.writeTxn(() => db.settings.put(settings));
+  }
+
+  // boolValue = test!.theme.toString();
+  final isDark = test!.isDark;
+
   runApp(
-    const ProviderScope(child: MyApp()),
+    //boolValue = test!.theme.toString();
+    ProviderScope(
+        child: MyApp(
+      boolValue: 'true',
+      isDark: isDark,
+    )),
   );
 }
 
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget {
+  final String? boolValue;
+  bool isDark;
+  MyApp({super.key, required this.boolValue, required this.isDark});
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ref.watch(ThemeProviderTest) == true
-          ? MyThemes.darkTheme
-          : MyThemes.lightTheme,
-      //themeMode: ThemeMode.system,
-      home: const MyhomePage(),
-    );
+  Widget build(BuildContext context) {
+    return Consumer(builder: (context, ref, child) {
+      //print(ref.watch(themeProviderTest));
+      // final lol;
+      // ref.watch(themeProviderTest).toString() == 'true'
+      //     ? lol = true
+      //     : lol = false;
+
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        // theme: lol == null ? MyThemes.darkTheme : MyThemes.lightTheme,
+
+        theme: ref
+                .watch(settingsStreamProvider)
+                .when(data: (data) {}, error: (e, t) {}, loading: () => {})
+            ? MyThemes.darkTheme
+            : MyThemes.lightTheme,
+        //themeMode: ThemeMode.system,
+        home: MyhomePage(
+          isDark: isDark,
+        ),
+      );
+    });
   }
 }
 
 class MyhomePage extends ConsumerWidget {
-  const MyhomePage({super.key});
+  MyhomePage({super.key, required this.isDark});
+  final bool isDark;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -40,22 +83,30 @@ class MyhomePage extends ConsumerWidget {
       appBar: AppBar(
         title: Text(text),
         leading: Switch.adaptive(
-          value: ref.watch(ThemeProviderTest),
-          onChanged: (value) {
-            ref.read(ThemeProviderTest.notifier).update((state) => value);
+          value: ref.watch(themeProviderTest) ?? isDark,
+          onChanged: (value) async {
+            final settings =
+                Settings(theme: value.toString(), id: 1, isDark: value);
+
+            await db.writeTxn(() => db.settings.put(settings));
+
+            final temp = await db.settings.get(1);
+            ref.read(themeProviderTest.notifier).state = temp!.isDark;
+            //debugPrint(temp!.theme.toString());
+
+            // ref.read(themeProvider.select((value) => temp!.theme.toString()));
+
+            // ref.read(themeProviderTest.notifier).state =
+            //     !ref.watch(themeProviderTest ?? true);
+
+            //IsarService().createTheme(Settings(theme: value.toString()));
           },
         ),
       ),
       body: Column(
         children: [
-          ElevatedButton(
-            onPressed: () {
-              ref.read(themeProvider.notifier).changeTheme();
-            },
-            child: const Text('data change'),
-          ),
           Center(
-            child: Text(ref.watch(ThemeProviderTest).toString()),
+            child: Text(ref.watch(themeProviderTest).toString()),
           ),
         ],
       ),
